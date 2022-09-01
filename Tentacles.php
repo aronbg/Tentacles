50cdc7c58ef7d04697a882fb404cd65eff8adc53
aronbg <64472189+aronbg@users.noreply.github.com>
2022-09-01 18:15:18 +0200

    Add files via upload

---
+++ Tentacles.php
@@ -0,0 +1,56 @@
+<!DOCTYPE HTML>
+<HTML>
+
+ <link href="SSTentacles1.css" rel="stylesheet" type="text/css" media="screen">
+<style>  
+    @import url('https://fonts.googleapis.com/css2?family=Russo+One');
+      h1 {
+        font-family: 'Russo One', sans-serif;
+      }
+        <BODY>
+    </style>
+    <div class= "rightbox"><H1 class="ClickTitle">Tentacles</H1></div>
+       <div class= "textDiv">
+           
+          <p><br>Multigenre bass party<br>
+Location: Carwash Utrecht<br>
+<span class= "date">17-9-22</span><br>
+            22:00 - 06:00<br>
+<span class= "important">free</span> entrance<br><br>
+              <span class= "important">8</span> Genres<br> <span class= "important">8</span> Sets
+           </p></div>
+    
+    <p1>Line up:<br>
+        <span class= "important">
+i1i</span>  -  Deep Dubstep<br> 
+        <span class= "important">
+            BorisBaap  </span>-  Funk House<br>
+
+         <span class= "important">
+Sorza</span>  -  Breakbeat DnB<br>
+<span class= "important">SEMPLE.</span>  -  Drum and Bounce<br>
+
+         <span class= "important">
+                Manximaal B2B Jager</span>  -  Dirty Trance<br>
+        <span class= "important">
+                Defazor B2B Macroscian</span>  -  Crossbreed/Darkstep<br>
+      
+</p1>
+   <div class = "Clickbox"> <a href="https://forms.gle/Zi3tJeUxb8WUtv3z8" target="_blank_parent"><h2>
+    Sign up!
+    
+        </h2>    </a></div>
+
+    
+    <p1>Expect:<br>
+DnB House Breakbeat Dubstep Psytrance Funk Crossbreed Breakcore
+    </p1>
+
+            
+
+        
+            
+    
+        
+            </BODY>
+</HTML>
