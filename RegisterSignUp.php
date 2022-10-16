7b9b85e81dd40951b222d4eba034cfe9cc0ab82a
aronbg <64472189+aronbg@users.noreply.github.com>
2022-10-17 01:24:51 +0200

    Update RegisterSignUp.php

--- RegisterSignUp.php
+++ RegisterSignUp.php
@@ -13,26 +13,6 @@ $query = "INSERT INTO TentaclesList(id, name, email) VALUES (NULL, '$name','$ema
 if (!($result = $mysqli->query($query)))
 showerror($mysqli->errno,$mysqli->error);
 
-$query = "SELECT * FROM users WHERE email = '$email'";
-
-if (!($result = $mysqli->query($query)))
-showerror($mysqli->errno,$mysqli->error);
-
-$row = $result->fetch_assoc();
-
-
-if($email==$row["email"]){
-    //echo session_id();
-    $_SESSION["email"]=$row["email"];
-    header('Location:LogOff.html');
-    //verder verwijzen naar device overzicht.
-echo "You're Already on the list";}
-else{
-    header('Location:index.html');
-    //terugverwijzen naar Register
-    
-}
-
 //echo json_encode($row["name"]) . json_encode["email"];
 
 ?>
